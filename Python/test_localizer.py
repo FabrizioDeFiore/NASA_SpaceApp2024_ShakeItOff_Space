d06e50e39afc272de2e02cd411278a58e98ef2a2
FabrizioDeFiore <fabriziodefiore@gmail.com>
2024-10-05 18:59:36 +0100

    Trying to test the training

--- Python/test_localizer.py
+++ Python/test_localizer.py
@@ -3,8 +3,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from dataset import EarthquakeDataset
-# from nets.localizer import FPN1DLocalizer as Localizer
+from datasetV2 import EarthquakeDataset
+#from nets.localizer import FPN1DLocalizer as Localizer
 from nets.localizer import SimpleCNNLocalizer as Localizer
 
 import psutil
@@ -17,19 +17,21 @@ def log_memory_usage():
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
-batch_size = 76
+batch_size = 64
 num_epochs = 2000
 learning_rate = 0.0001
 
 # Dataset and DataLoader
 test_dataset = EarthquakeDataset(data_folder='data/lunar/test/downsample_data/S12_GradeB/',
-                                  #label_folder=None, is_testing=True)
-                                  is_testing=True)
+                                  label_folder=None, is_testing=True)
 
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 # Model
-model = Localizer(num_layers=7, in_channels=1, mid_channels=8, kernel_size=13).to(device)
+#model = Localizer(num_layers=7, in_channels=1, mid_channels=8, kernel_size=13).to(device)
+model = Localizer(num_layers=3, in_channels=1, mid_channels=64, kernel_size=3).to(device)
+#model = Localizer(num_pools=10, in_channels=1, mid_channels=64, kernel_size=31).to(device)
+
 
 # Load the saved model parameters
 model.load_state_dict(torch.load('save/localizer/trial_2/quake_localization_model_00930.pth'))
